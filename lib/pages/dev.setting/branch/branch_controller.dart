dac8e6bd01c05eec433fa7ec300f6ab18b87affb
code-bross <rkd2468@icloud.com>
2022-03-24 09:50:26 +0900

    - branch add function

--- lib/pages/dev.setting/branch/branch_controller.dart
+++ lib/pages/dev.setting/branch/branch_controller.dart
@@ -1,12 +1,379 @@
-import 'package:chat_bubbles/chat_bubbles.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:timeline_list/timeline_model.dart';
+import 'package:intl/intl.dart';
+import 'package:kt_dart/collection.dart';
 
 class BranchController extends GetxController {
+  KtMutableList<Feed> _items = KtMutableList.of(
+    Feed(
+        createdAt: DateTime.utc(2022, 2, 23, 14, 25),
+        content: '씨에스윈드, 종속회사 ASM Industries 지분 추가 취득 결정',
+        header: Header(
+            title: '국내뉴스',
+            contentType: ContentType.News,
+            predictionType: PredictionType.Bear,
+            tags: []),
+        expand: false,
+        comment: Comment(
+            comment: '독자 경영권 확보했고, 제무제푶 상 매출 올라 갈것',
+            thumbsUpcount: 12,
+            commentCount: 6)),
+    Feed(
+        createdAt: DateTime.utc(2022, 2, 23, 14, 23),
+        content: '전운에 씨에스윈드, 부각되네.....',
+        header: Header(
+            title: '국내뉴스',
+            contentType: ContentType.News,
+            predictionType: PredictionType.Bear,
+            tags: []),
+        expand: false,
+        comment: null),
+    Feed(
+        createdAt: DateTime.utc(2022, 2, 23, 14, 21),
+        content: '러시아 리스크 풍력주 기회',
+        header: Header(
+            title: '해외뉴스',
+            contentType: ContentType.News,
+            predictionType: PredictionType.Bull,
+            tags: []),
+        expand: false,
+        comment: null),
+    Feed(
+        createdAt: DateTime.utc(2022, 2, 22, 11, 09),
+        content: '평단 5.6 주주입니다. 상방으로 가려면...',
+        header: Header(
+            title: '코멘트',
+            contentType: ContentType.Comment,
+            predictionType: PredictionType.Bull,
+            tags: ['#주주인증']),
+        expand: false,
+        comment: Comment(
+            comment:
+                '거래량은 한 번 터져야 된다고 생각합니다.\n몇 달 째 연이은 하락으로 조금씩 매수를 해나가고 있습니다.',
+            thumbsUpcount: 12,
+            commentCount: 6)),
+  );
+
+  KtList<Feed> get items {
+    return _items;
+  }
+
+  KtMap<String, KtList<Feed>> get group {
+    return items
+        .sortedByDescending((p0) => p0.createdAt)
+        .groupBy((it) => '${it.createdAt.month}/${it.createdAt.day}');
+  }
+
   @override
   void onInit() {
     super.onInit();
   }
+
+  void expand(int parentIndex, Feed feed) {
+    var index = items.indexOf(feed);
+    _items[index] = feed.copyWith(expand: !feed.expand);
+    update();
+  }
+
+  void add() {
+    _items.add(Feed(
+        createdAt: DateTime.now(),
+        content: '아이템 추가',
+        header: Header(
+            title: '국내뉴스',
+            contentType: ContentType.News,
+            predictionType: PredictionType.Bear,
+            tags: []),
+        expand: false,
+        comment: Comment(comment: '추가', thumbsUpcount: 12, commentCount: 6)));
+    update();
+  }
 }
+
+class Content {
+  final String title;
+  final List<Feed> feeds;
+
+//<editor-fold desc="Data Methods">
+
+  const Content({
+    required this.title,
+    required this.feeds,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Content &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          feeds == other.feeds);
+
+  @override
+  int get hashCode => title.hashCode ^ feeds.hashCode;
+
+  @override
+  String toString() {
+    return 'Content{' + ' title: $title,' + ' messages: $feeds,' + '}';
+  }
+
+  Content copyWith({
+    String? title,
+    List<Feed>? messages,
+  }) {
+    return Content(
+      title: title ?? this.title,
+      feeds: messages ?? this.feeds,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': this.title,
+      'messages': this.feeds,
+    };
+  }
+
+  factory Content.fromMap(Map<String, dynamic> map) {
+    return Content(
+      title: map['title'] as String,
+      feeds: map['messages'] as List<Feed>,
+    );
+  }
+
+//</editor-fold>
+}
+
+class Feed {
+  final DateTime createdAt; // final DateTime createdAt;
+  final String content;
+  final Header header;
+  final bool expand;
+  final Comment? comment;
+
+  String timeString() => DateFormat('HH:mm').format(createdAt);
+
+//<editor-fold desc="Data Methods">
+
+  const Feed({
+    required this.createdAt,
+    required this.content,
+    required this.header,
+    required this.expand,
+    required this.comment,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Feed &&
+          runtimeType == other.runtimeType &&
+          createdAt == other.createdAt &&
+          content == other.content &&
+          header == other.header &&
+          expand == other.expand &&
+          comment == other.comment);
+
+  @override
+  int get hashCode =>
+      createdAt.hashCode ^
+      content.hashCode ^
+      header.hashCode ^
+      expand.hashCode ^
+      comment.hashCode;
+
+  @override
+  String toString() {
+    return 'Feed{' +
+        ' createdAt: $createdAt,' +
+        ' content: $content,' +
+        ' header: $header,' +
+        ' expand: $expand,' +
+        ' comment: $comment,' +
+        '}';
+  }
+
+  Feed copyWith({
+    DateTime? createdAt,
+    String? content,
+    Header? header,
+    bool? expand,
+    Comment? comment,
+  }) {
+    return Feed(
+      createdAt: createdAt ?? this.createdAt,
+      content: content ?? this.content,
+      header: header ?? this.header,
+      expand: expand ?? this.expand,
+      comment: comment ?? this.comment,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'createdAt': this.createdAt,
+      'content': this.content,
+      'header': this.header,
+      'expand': this.expand,
+      'comment': this.comment,
+    };
+  }
+
+  factory Feed.fromMap(Map<String, dynamic> map) {
+    return Feed(
+      createdAt: map['createdAt'] as DateTime,
+      content: map['content'] as String,
+      header: map['header'] as Header,
+      expand: map['expand'] as bool,
+      comment: map['comment'] as Comment,
+    );
+  }
+
+//</editor-fold>
+}
+
+class Header {
+  final String title;
+  final ContentType contentType;
+  final PredictionType predictionType;
+  final List<String> tags;
+
+//<editor-fold desc="Data Methods">
+
+  const Header({
+    required this.title,
+    required this.contentType,
+    required this.predictionType,
+    required this.tags,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Header &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          contentType == other.contentType &&
+          predictionType == other.predictionType &&
+          tags == other.tags);
+
+  @override
+  int get hashCode =>
+      title.hashCode ^
+      contentType.hashCode ^
+      predictionType.hashCode ^
+      tags.hashCode;
+
+  @override
+  String toString() {
+    return 'Header{' +
+        ' title: $title,' +
+        ' contentType: $contentType,' +
+        ' predictionType: $predictionType,' +
+        ' tags: $tags,' +
+        '}';
+  }
+
+  Header copyWith({
+    String? title,
+    ContentType? contentType,
+    PredictionType? predictionType,
+    List<String>? tags,
+  }) {
+    return Header(
+      title: title ?? this.title,
+      contentType: contentType ?? this.contentType,
+      predictionType: predictionType ?? this.predictionType,
+      tags: tags ?? this.tags,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': this.title,
+      'contentType': this.contentType,
+      'predictionType': this.predictionType,
+      'tags': this.tags,
+    };
+  }
+
+  factory Header.fromMap(Map<String, dynamic> map) {
+    return Header(
+      title: map['title'] as String,
+      contentType: map['contentType'] as ContentType,
+      predictionType: map['predictionType'] as PredictionType,
+      tags: map['tags'] as List<String>,
+    );
+  }
+
+//</editor-fold>
+}
+
+class Comment {
+  final String comment;
+  final int commentCount;
+  final int thumbsUpcount;
+
+//<editor-fold desc="Data Methods">
+
+  const Comment({
+    required this.comment,
+    required this.commentCount,
+    required this.thumbsUpcount,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Comment &&
+          runtimeType == other.runtimeType &&
+          comment == other.comment &&
+          commentCount == other.commentCount &&
+          thumbsUpcount == other.thumbsUpcount);
+
+  @override
+  int get hashCode =>
+      comment.hashCode ^ commentCount.hashCode ^ thumbsUpcount.hashCode;
+
+  @override
+  String toString() {
+    return 'Comment{' +
+        ' comment: $comment,' +
+        ' commentCount: $commentCount,' +
+        ' thumbsUpcount: $thumbsUpcount,' +
+        '}';
+  }
+
+  Comment copyWith({
+    String? comment,
+    int? commentCount,
+    int? thumbsUpcount,
+  }) {
+    return Comment(
+      comment: comment ?? this.comment,
+      commentCount: commentCount ?? this.commentCount,
+      thumbsUpcount: thumbsUpcount ?? this.thumbsUpcount,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'comment': this.comment,
+      'commentCount': this.commentCount,
+      'thumbsUpcount': this.thumbsUpcount,
+    };
+  }
+
+  factory Comment.fromMap(Map<String, dynamic> map) {
+    return Comment(
+      comment: map['comment'] as String,
+      commentCount: map['commentCount'] as int,
+      thumbsUpcount: map['thumbsUpcount'] as int,
+    );
+  }
+
+//</editor-fold>
+}
+
+enum PredictionType { Bull, Bear }
+
+enum ContentType { News, Comment }
--- lib/pages/dev.setting/branch/branch_page.dart
+++ lib/pages/dev.setting/branch/branch_page.dart
@@ -1,22 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:kt_dart/collection.dart';
+import 'package:kt_dart/kt.dart';
 import 'package:meta_vest_sample/pages/dev.setting/branch/branch_controller.dart';
 import 'package:timelines/timelines.dart';
 
+class BranchPage extends StatelessWidget {
+  const BranchPage({Key? key}) : super(key: key);
+
+  Widget _body(BuildContext context, KtMap<String, KtList<Feed>> map) {
+    return SingleChildScrollView(
+        child: FixedTimeline.tileBuilder(
+            theme: TimelineThemeData(
+              nodePosition: 0,
+              color: Color(0xff989898),
+              indicatorTheme: IndicatorThemeData(
+                position: 0,
+                size: 20.0,
+              ),
+              connectorTheme: ConnectorThemeData(
+                thickness: 2.5,
+              ),
+            ),
+            builder: TimelineTileBuilder.connected(
+                connectionDirection: ConnectionDirection.before,
+                itemCount: map.size,
+                contentsBuilder: (_, index) {
+                  var key = map.keys.toList()[index];
+
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          key,
+                          style: TextStyle(
+                              color: Colors.white,
+                              backgroundColor: Colors.black),
+                        ),
+                        _InnerTimeline(
+                            parentIndex: index,
+                            feeds: map[key] ?? KtList.empty()),
+                      ],
+                    ),
+                  );
+                })));
+  }
+
+  @override
+  Widget build(context) {
+    return GetBuilder<BranchController>(
+        builder: (controller) => Scaffold(
+              appBar: AppBar(
+                title: Text('씨에스윈드'),
+              ),
+              body: _body(context, controller.group),
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: ()=>{
+                    controller.add()
+                },
+              ),
+            ));
+  }
+}
+
 class _InnerTimeline extends StatelessWidget {
   const _InnerTimeline({
+    required this.parentIndex,
     required this.feeds,
   });
 
-  final List<_Feed> feeds;
+  final int parentIndex;
+
+  final KtList<Feed> feeds;
 
   @override
   Widget build(BuildContext context) {
     bool isEdgeIndex(int index) {
-      return index == 0 || index == feeds.length + 1;
+      return index == 0 || index == feeds.size + 1;
     }
 
     return Padding(
@@ -33,8 +98,18 @@ class _InnerTimeline extends StatelessWidget {
               ),
         ),
         builder: TimelineTileBuilder(
-          indicatorBuilder: (_, index) =>
-              !isEdgeIndex(index) ? Indicator.outlined(borderWidth: 2.0) : null,
+          indicatorBuilder: (_, index) => !isEdgeIndex(index)
+              ? Indicator.outlined(
+                  borderWidth: 0,
+                  backgroundColor: feeds[index - 1].header.predictionType ==
+                          PredictionType.Bull
+                      ? Colors.red
+                      : Colors.blue,
+                  color: feeds[index - 1].header.predictionType ==
+                          PredictionType.Bull
+                      ? Colors.red
+                      : Colors.blue)
+              : null,
           startConnectorBuilder: (_, index) => Connector.solidLine(),
           endConnectorBuilder: (_, index) => Connector.solidLine(),
           contentsBuilder: (_, index) {
@@ -44,189 +119,127 @@ class _InnerTimeline extends StatelessWidget {
 
             var feed = feeds[index - 1];
             return Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Column(
-                children: [
-                  Row(
+                padding: EdgeInsets.only(left: 8.0),
+                child: InkWell(
+                  onTap: () => feed.comment?.let((_) {
+                    return Get.find<BranchController>()
+                        .expand(parentIndex, feed);
+                  }),
+                  child: Column(
                     children: [
-                      Text(feed.timeString()),
-                      feed.header.contentType == _ContentType.Comment
-                          ? Container(
-                              child: Text(
-                                feed.header.title,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                              ),
-                              margin: EdgeInsets.only(left: 4),
-                              padding: EdgeInsets.all(4),
-                            )
-                          : Container(
-                              child: Text(feed.header.title),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  width: 1,
-                                  color: Colors.black,
+                      Row(
+                        children: [
+                          Text(feed.timeString()),
+                          feed.header.contentType == ContentType.Comment
+                              ? Container(
+                                  child: Text(
+                                    feed.header.title,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                  ),
+                                  margin: EdgeInsets.only(left: 4),
+                                  padding: EdgeInsets.all(4),
+                                )
+                              : Container(
+                                  child: Text(feed.header.title),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      width: 1,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  margin: EdgeInsets.only(left: 4),
+                                  padding: EdgeInsets.all(4),
                                 ),
-                              ),
-                              margin: EdgeInsets.only(left: 4),
-                              padding: EdgeInsets.all(4),
+                          feed.header.predictionType == PredictionType.Bear
+                              ? Icon(
+                                  Icons.arrow_drop_up,
+                                  color: Colors.red,
+                                )
+                              : Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Colors.blue,
+                                ),
+                          feed.header.tags.isNotEmpty
+                              ? Row(
+                                  children: feed.header.tags
+                                      .map((e) => Text(
+                                            e,
+                                            style: TextStyle(fontSize: 11),
+                                          ))
+                                      .toList(),
+                                )
+                              : Container()
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                          child: Column(
+                        children: [
+                          Container(
+                            child: Text(
+                              feed.content,
                             ),
-                      feed.header.predictionType == _PredictionType.Bear
-                          ? Icon(
-                              Icons.arrow_drop_up,
-                              color: Colors.red,
-                            )
-                          : Icon(
-                              Icons.arrow_drop_down,
-                              color: Colors.blue,
-                            ),
-                      feed.header.tags.isNotEmpty
-                          ? Row(
-                              children: feed.header.tags
-                                  .map((e) => Text(
-                                        e,
-                                        style: TextStyle(fontSize: 11),
-                                      ))
-                                  .toList(),
-                            )
-                          : Container()
+                            alignment: Alignment.centerLeft,
+                          ),
+                          feed.expand && feed.comment != null
+                              ? Column(children: [
+                                  Container(
+                                    alignment: Alignment.centerRight,
+                                    padding: EdgeInsets.all(8),
+                                    margin: EdgeInsets.only(top: 4, right: 8),
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        borderRadius: BorderRadius.circular(8)),
+                                    child: Container(
+                                      child: Text(
+                                        feed.comment?.comment ?? '',
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      alignment: Alignment.centerLeft,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(right: 8),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Icon(Icons.thumb_up_alt_outlined),
+                                        Text(feed.comment?.thumbsUpcount
+                                                .toString() ??
+                                            ''),
+                                        Icon(
+                                            Icons.chat_bubble_outline_outlined),
+                                        Text(feed.comment?.commentCount
+                                                .toString() ??
+                                            ''),
+                                      ],
+                                    ),
+                                    alignment: Alignment.centerRight,
+                                  )
+                                ])
+                              : Container()
+                        ],
+                      )),
+                      SizedBox(
+                        height: 20,
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                      child: Container(
-                    child: Text(
-                      feed.content,
-                    ),
-                    alignment: Alignment.centerLeft,
-                  ))
-                ],
-              ),
-            );
+                ));
           },
           nodeItemOverlapBuilder: (_, index) =>
               isEdgeIndex(index) ? true : null,
-          itemCount: feeds.length + 2,
+          itemCount: feeds.size + 2,
         ),
       ),
     );
   }
 }
-
-class BranchPage extends StatelessWidget {
-  const BranchPage({Key? key}) : super(key: key);
-
-  Widget _body(BuildContext context) {
-    var processes = [
-      _Content(
-        '2/23',
-        messages: [
-          _Feed(
-              DateTime.utc(2022, 2, 23, 14, 23),
-              '씨에스윈드, 종속회사 ASM Industries 지분 추가 취득 결정',
-              _Header('국내뉴스', _ContentType.News, _PredictionType.Bear, [])),
-        ],
-      ),
-      _Content(
-        '2/22',
-        messages: [
-          _Feed(DateTime.utc(2022, 2, 23, 14, 23), '전운에 씨에스윈드, 부각되네.....',
-              _Header('국내뉴스', _ContentType.News, _PredictionType.Bear, [])),
-          _Feed(DateTime.utc(2022, 2, 23, 14, 21), '러시아 리스크 풍력주 기회',
-              _Header('해외뉴스', _ContentType.News, _PredictionType.Bull, [])),
-          _Feed(
-              DateTime.utc(2022, 2, 23, 11, 09),
-              '평단 5.6 주주입니다. 상방으로 가려면...',
-              _Header('코멘트', _ContentType.Comment, _PredictionType.Bull,
-                  ['#주주인증'])),
-        ],
-      )
-    ];
-
-    return FixedTimeline.tileBuilder(
-        theme: TimelineThemeData(
-          nodePosition: 0,
-          color: Color(0xff989898),
-          indicatorTheme: IndicatorThemeData(
-            position: 0,
-            size: 20.0,
-          ),
-          connectorTheme: ConnectorThemeData(
-            thickness: 2.5,
-          ),
-        ),
-        builder: TimelineTileBuilder.connected(
-            connectionDirection: ConnectionDirection.before,
-            itemCount: processes.length,
-            contentsBuilder: (_, index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      processes[index].name,
-                      style: TextStyle(
-                          color: Colors.white, backgroundColor: Colors.black),
-                    ),
-                    _InnerTimeline(feeds: processes[index].messages),
-                  ],
-                ),
-              );
-            }));
-  }
-
-  @override
-  Widget build(context) {
-    return GetBuilder<BranchController>(
-        builder: (controller) => Scaffold(
-              appBar: AppBar(
-                title: Text('씨에스윈드'),
-              ),
-              body: _body(context),
-            ));
-  }
-}
-
-class _Content {
-  const _Content(
-    this.name, {
-    this.messages = const [],
-  });
-
-  final String name;
-  final List<_Feed> messages;
-}
-
-class _Feed {
-  const _Feed(this.createdAt, this.content, this.header);
-
-  final DateTime createdAt; // final DateTime createdAt;
-  final String content;
-  final _Header header;
-
-  @override
-  String toString() {
-    return '${timeString()} $content';
-  }
-
-  String timeString() => DateFormat('HH:mm').format(createdAt);
-}
-
-class _Header {
-  const _Header(this.title, this.contentType, this.predictionType, this.tags);
-
-  final String title;
-  final _ContentType contentType;
-  final _PredictionType predictionType;
-  final List<String> tags;
-}
-
-enum _PredictionType { Bull, Bear }
-
-enum _ContentType { News, Comment }
